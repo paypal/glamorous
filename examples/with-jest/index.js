846ff73a3ee612ccc4e5d8756548266838b55dfa
Pavithra K <pksjce@gmail.com>
2017-04-09 20:59:11 +0530

    fix(example): Add to .eslintignore
    Update test file to directly export glamorous components

--- examples/with-jest/index.js
+++ examples/with-jest/index.js
@@ -1,19 +1,12 @@
-import React from 'react'
-import glamorous from '../../src'
+import glamorous from 'glamorous'
 
-export function Wrapper(props) {
-  const MySection = glamorous.section({
-    padding: '4em',
-    background: 'papayawhip',
-  })
-  return <MySection {...props} />
-}
+export const Wrapper = glamorous.section({
+  padding: '4em',
+  background: 'papayawhip',
+})
 
-export function Title(props) {
-  const MyH1 = glamorous.h1({
-    fontSize: '1.5em',
-    textAlign: 'center',
-    color: 'palevioletred',
-  })
-  return <MyH1 {...props} />
-}
+export const Title = glamorous.h1({
+  fontSize: '1.5em',
+  textAlign: 'center',
+  color: 'palevioletred',
+})
