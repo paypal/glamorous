140840f404c818887930a575c8a5faa36b756fcf
Kok J Sam <samkj.ks@gmail.com>
2017-04-14 12:29:50 -0400

    fix($typings): Update Typescript definitions
    Some of the TS defintions don't match with current implementation. For example, `glamorous` takes in
    any number of style object and function. But the type definition only allows two arguments. Also,
    definition for `glamorous` returns incorrect type.

--- typings/glamorous.test.tsx
+++ typings/glamorous.test.tsx
@@ -1,21 +1,94 @@
-import glamorous from "../";
+import * as React from "react";
+import glamorous, { ThemeProvider } from "../";
 
 // static styles
-const MyStyledDiv = glamorous.div({
-  fontSize: 20,
-  textAlign: 'center',
+const Static = glamorous.div({
+  "fontSize": 20,
+  "textAlign": "center",
 });
 
 // dynamic styles
-const Title = glamorous.h1({
-  fontSize: '10px'
-}, (props: { color: string; }) => ({
-  color: props.color,
-}));
+const Title = glamorous.h1(
+  {
+    "fontSize": "10px",
+  },
+  (props: { color: string; }) => ({
+    "color": props.color,
+  }),
+);
 
 // theme styles
-const Divider = glamorous.span({
-  fontSize: '10px'
-}, (props, theme: { main: { color: string; }}) => ({
-  color: theme.main.color,
-}));
+const Divider = glamorous.span(
+  {
+    "fontSize": "10px",
+  },
+  (props, theme: { main: { color: string; }}) => ({
+    "color": theme.main.color,
+  }),
+);
+
+// n-number of styles
+const SpanDivider = glamorous.span(
+  {
+    "fontSize": "10px",
+  },
+  (props, theme) => ({
+    "color": theme,
+  }),
+  {
+    "fontWeight": 500,
+  },
+  {
+    "fontFamily": "Roboto",
+    "fontWeight": 500,
+  },
+  (props, theme: { main: { color: string; }}) => ({
+    "color": theme.main.color,
+  }),
+);
+
+interface DividerInsideDividerProps {
+  color: string;
+};
+
+// component styles
+const DividerInsideDivider = glamorous(Divider)(
+  {
+    "fontSize": "10px",
+  },
+  (props, theme: { main: { color: string; }}) => ({
+    "color": theme.main.color,
+  }),
+);
+
+const theme = {
+  "main": {
+    "color": "red",
+  },
+};
+
+export const Balloon = () => (
+  <ThemeProvider theme={theme}>
+    <Divider color="blue">
+      <DividerInsideDivider color="blue">
+        <Static>Static</Static>
+        <Title>Hello</Title>
+      </DividerInsideDivider>
+    </Divider>
+  </ThemeProvider>
+);
+
+export class AirBalloon extends React.Component<{}, {}> {
+  private spanElem: HTMLSpanElement;
+
+  public render() {
+    return (
+      <Divider ref={(c) => { this.spanElem = c; }}>
+        Hello
+        <SpanDivider>
+          Span Divider
+        </SpanDivider>
+      </Divider>
+    );
+  }
+}
