4af0c43c58bb0774a126150314ae8a659802e88e
Kent C. Dodds <kent+github@doddsfamily.us>
2017-07-10 14:22:43 -0600

    Update tiny.js

--- src/__tests__/tiny.js
+++ src/__tests__/tiny.js
@@ -28,7 +28,7 @@ test('should pass glam object prop', () => {
   const props = {
     glam,
     id: 'hey-there',
-    theme, // this is just insidental because we have a theme prop
+    theme,
   }
   const context = expect.any(Object) // the context
   expect(dynamicStyles).toHaveBeenCalledWith(props, context)
