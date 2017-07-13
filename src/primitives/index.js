82c608f416bd7d8a021073f1a6fae90fe71c7d98
Nitin <tulswani19@gmail.com>
2017-07-13 12:48:42 +0530

    export primitive constructor

---
+++ src/primitives/index.js
@@ -0,0 +1,33 @@
+import reactPrimitives from 'react-primitives'
+import glamorous, {ThemeProvider, withTheme} from '../index'
+
+// Create glamorous component with composition
+function createComponent(componentConstructor, tag) {
+  if (typeof tag !== 'string' && typeof tag !== 'function') {
+    throw new Error(`Cannot create glamorous component for tag ${tag}`)
+  }
+
+  return componentConstructor(tag)
+}
+
+// Render the primitive React interfaces with glamorousComponent()
+const glamorousComponent = tag => createComponent(glamorous, tag)
+
+// Native UI components
+const aliases = 'Image Text Touchable View'
+
+/* Lowercase the aliases according to the built-in React Native component factories,
+* define the getter for each alias and then pass it to glamorousComponent */
+
+aliases.split(/\s+/m).forEach(alias =>
+  Object.defineProperty(glamorousComponent, alias.toLowerCase(), {
+    enumerable: true,
+    configurable: false,
+    get() {
+      return glamorousComponent(reactPrimitives[alias])
+    },
+  }),
+)
+
+export default glamorousComponent
+export {ThemeProvider, withTheme}
