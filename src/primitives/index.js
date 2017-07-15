bac2843de857c7e67af53899a339a9e27425fd66
Nitin <tulswani19@gmail.com>
2017-07-15 13:35:11 +0530

    some changes

--- src/primitives/index.js
+++ src/primitives/index.js
@@ -2,34 +2,20 @@ import reactPrimitives from 'react-primitives'
 import glamorous, {ThemeProvider, withTheme} from '../index'
 
 // Create glamorous component with composition
-function createComponent(componentConstructor, tag) {
-  if (typeof tag !== 'string' && typeof tag !== 'function') {
-    throw new Error(`Cannot create glamorous component for tag ${tag}`)
-  }
-
-  return componentConstructor(tag)
-}
-
-// Render the primitive React interfaces with glamorousComponent()
-function glamorousComponent(tag) {
-  return createComponent(glamorous, tag)
-}
+let glamorousComponent = tag => glamorous(tag)
 
 // Native UI components
-const aliases = 'Image Text Touchable View'
+const aliases = ['Image', 'Text', 'Touchable', 'View']
 
 /* Lowercase the aliases according to the built-in React Native component
 factories, define the getter for each alias and then pass it to
 glamorousComponent */
 
-aliases.split(/\s+/m).forEach(alias =>
-  Object.defineProperty(glamorousComponent, alias.toLowerCase(), {
-    enumerable: true,
-    configurable: false,
-    get() {
-      return glamorousComponent(reactPrimitives[alias])
-    },
-  }),
+aliases.forEach(
+  alias =>
+    (glamorousComponent[alias.toLowerCase()] = glamorousComponent(
+      reactPrimitives[alias],
+    )),
 )
 
 export default glamorousComponent
