96993c4ef91027c05e715cb0c2ad64327f2f5a0d
Nitin <tulswani19@gmail.com>
2017-07-26 00:06:54 +0530

    some changes

--- src/primitives/index.js
+++ src/primitives/index.js
@@ -1,22 +1,20 @@
 import reactPrimitives from 'react-primitives'
 import glamorous, {ThemeProvider, withTheme} from '../index'
 
-// Create glamorous component with composition
-let glamorousComponent = tag => glamorous(tag)
-
 // Native UI components
 const aliases = ['Image', 'Text', 'Touchable', 'View']
 
 /* Lowercase the aliases according to the built-in React Native component
 factories, define the getter for each alias and then pass it to
 glamorousComponent */
-
-aliases.forEach(
-  alias =>
-    (glamorousComponent[alias.toLowerCase()] = glamorousComponent(
-      reactPrimitives[alias],
-    )),
+Object.assign(
+  glamorous,
+  aliases.reduce((getters, alias) => {
+    const tag = alias.toLowerCase()
+    getters[tag] = glamorous(reactPrimitives[alias])
+    return getters
+  }, {}),
 )
 
-export default glamorousComponent
+export default glamorous
 export {ThemeProvider, withTheme}
