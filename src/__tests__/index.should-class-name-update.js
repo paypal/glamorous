dabb612d316ed1303dd2304721bc7e391f4ec544
Dylan Mozlowski <dmozlowski@classy.org>
2017-08-02 17:25:36 -0700

    fix: Fix shouldClassNameUpdate to track className per instance, not per component

--- src/__tests__/index.should-class-name-update.js
+++ src/__tests__/index.should-class-name-update.js
@@ -39,22 +39,91 @@ test('calls shouldClassNameUpdate and updates accordingly', () => {
   // let's make sure that we're calling the function
   // with the right args.
   const props = expect.objectContaining({
-    css: {marginLeft: 3},
-    id: 'hi there',
-  })
-  const prevProps = expect.objectContaining({
     id: 'goodbye',
     css: {marginLeft: 2},
+  })
+  const nextProps = expect.objectContaining({
+    css: {marginLeft: 3},
+    id: 'hi there',
   })
   // ok ok, I'm not really testing `context` very thoroughly,
   // but can you blame me? It's so totally boring...
   // and I'm pretty sure it's working fine
   const context = expect.objectContaining({__glamorous__: undefined})
-  const prevContext = expect.objectContaining({__glamorous__: undefined})
+  const nextContext = expect.objectContaining({__glamorous__: undefined})
   expect(shouldClassNameUpdate).toHaveBeenCalledWith(
     props,
-    prevProps,
+    nextProps,
     context,
-    prevContext,
+    nextContext,
   )
+})
+
+test('shouldClassNameUpdate is specific to the component instance', () => {
+  const shouldClassNameUpdate = jest.fn()
+
+  const Container = ({children, ...props}) => {
+    return (
+      <div {...props}>
+        {children()}
+      </div>
+    )
+  }
+
+  const Div = glamorous('div', {
+    shouldClassNameUpdate,
+  })({
+    padding: 10,
+  })
+
+  // On the third render (second update()), change the color props
+  let count = 0
+  const wrapper = mount(
+    <Container>
+      {() => {
+        count++
+
+        return (
+          <div>
+            <Div color={count === 3 ? 'purple' : 'red'} />
+            <Div color={count === 3 ? 'green' : 'blue'} />
+          </div>
+        )
+      }}
+    </Container>,
+  )
+
+  expect(shouldClassNameUpdate).toHaveBeenCalledTimes(0)
+
+  wrapper.update()
+
+  expect(shouldClassNameUpdate).toHaveBeenCalledTimes(2)
+  expect(shouldClassNameUpdate.mock.calls[0]).toEqual([
+    expect.objectContaining({color: 'red'}),
+    expect.objectContaining({color: 'red'}),
+    expect.objectContaining({__glamorous__: undefined}),
+    expect.objectContaining({__glamorous__: undefined}),
+  ])
+  expect(shouldClassNameUpdate.mock.calls[1]).toEqual([
+    expect.objectContaining({color: 'blue'}),
+    expect.objectContaining({color: 'blue'}),
+    expect.objectContaining({__glamorous__: undefined}),
+    expect.objectContaining({__glamorous__: undefined}),
+  ])
+
+  wrapper.update()
+
+  expect(shouldClassNameUpdate).toHaveBeenCalledTimes(4)
+  expect(shouldClassNameUpdate.mock.calls[2]).toEqual([
+    expect.objectContaining({color: 'red'}),
+    expect.objectContaining({color: 'purple'}),
+    expect.objectContaining({__glamorous__: undefined}),
+    expect.objectContaining({__glamorous__: undefined}),
+  ])
+  expect(shouldClassNameUpdate.mock.calls[3]).toEqual([
+    expect.objectContaining({color: 'blue'}),
+    expect.objectContaining({color: 'green'}),
+    expect.objectContaining({__glamorous__: undefined}),
+    expect.objectContaining({__glamorous__: undefined}),
+  ])
 })
