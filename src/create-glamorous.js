6f840a462cb7f2ec0e601faa02e43e614cfed160
Mordy Tikotzky <mordytk@gmail.com>
2018-01-18 19:32:39 -0500

    update to use getRootEl

--- src/create-glamorous.js
+++ src/create-glamorous.js
@@ -114,7 +114,7 @@ function createGlamorous(splitProps) {
           {
             ...componentProperties,
             comp: newComp,
-            rootEl: newComp,
+            rootEl: getRootEl(newComp),
           },
           {
             // allows the forwardProps and filterProps to be overridden
