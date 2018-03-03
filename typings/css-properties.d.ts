25a7ad639ccabdc3edb4ac5f0eb03dbb73a444e4
Dan Schuman <dan.schuman@instrument.com>
2018-03-02 18:02:33 -0800

    Add CSSFunctions to CSSProperties typings

--- typings/css-properties.d.ts
+++ typings/css-properties.d.ts
@@ -1933,6 +1933,14 @@ export interface CSSProperties
     CSSPropertiesPseudo,
     CSSPropertiesLossy {}
 
-export type CSSPropertiesRecursive = CSSProperties | CSSPropertiesArray
+export type CSSPropertiesRecursive =
+  | CSSProperties
+  | CSSPropertiesArray
+  | CSSFunction
+
+// TODO: This could be made generic. Issue PR if you're so inclined!
+export interface CSSFunction {
+  (props: {}): CSSPropertiesRecursive
+}
 
 export interface CSSPropertiesArray extends Array<CSSPropertiesRecursive> {}
