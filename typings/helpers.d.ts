76c2e1eb48933d8f3959cc2a3d1c98e3ec967bd0
Aaron Jensen <aaronjensen@gmail.com>
2018-05-31 15:00:49 -0700

    Fix typings for Typescript 2.9

--- typings/helpers.d.ts
+++ typings/helpers.d.ts
@@ -1,5 +1,7 @@
-type Diff<T extends string, U extends string> = ({[P in T]: P} &
-  {[P in U]: never} & {[x: string]: never})[T]
+type Diff<
+  T extends string | number | symbol,
+  U extends string | number | symbol
+> = ({[P in T]: P} & {[P in U]: never} & {[x: string]: never})[T]
 
 export type Omit<T, K extends keyof T> = Pick<T, Diff<keyof T, K>>
 
